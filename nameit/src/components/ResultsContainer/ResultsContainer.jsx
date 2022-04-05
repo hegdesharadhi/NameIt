e0066bcb7fa5419bba77a22949de2f5cf41d8eb3
Sharadhi Hegde <hegdesharadhi@gmail.com>
2022-04-05 12:53:08 +0530

    initial commit

---
+++ nameit/src/components/ResultsContainer/ResultsContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ResultsContainer.css';
+import NameCard from './../NameCard/NameCard.jsx';
+
+const ResultsContainer =({suggestedNames})=>{
+    const suggestedNamesJSX = suggestedNames.map(suggestedName=>{
+        return <NameCard key={suggestedName} suggestedName={suggestedName} />
+
+    })
+    return (<div className="results-container">{suggestedNamesJSX}
+    </div>)
+}
+
+export default ResultsContainer;
